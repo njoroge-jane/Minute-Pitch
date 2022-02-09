2dcb8d06e5dfc5295a29594347ca0ffd41114432
Jane Njoroge <jane.njoroge@student.moringaschool.com>
2022-02-09 08:28:48 +0300

    add flask migrate modules

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,20 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from config import config_options
 
+db = SQLAlchemy()
 def create_app(config_name):
     app = Flask(__name__)
 
     # Creating the app configurations
-    app.config.from_object(config_options[config_name]) 
+    app.config.from_object(config_options[config_name])
 
-
-        # Registering the blueprint
+    # Registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
+    # Initializing
+    
+    db.init_app(app)
 
     return app
