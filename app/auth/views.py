0772176626d2d5322154b7ceb2587c9831ab42e8
Jane Njoroge <jane.njoroge@student.moringaschool.com>
2022-02-11 03:00:33 +0300

    add email sending function

--- app/auth/views.py
+++ app/auth/views.py
@@ -4,6 +4,7 @@ from flask_login import login_user,logout_user,login_required
 from .forms import ReviewForm,RegistrationForm,LoginForm
 from ..models import User
 from .. import db
+from ..email import mail_message
 
 
 # @auth.route('/login',  methods = ['GET','POST'])
@@ -31,6 +32,7 @@ def register():
         user = User(email = form.email.data, username = form.username.data,password = form.password.data)
         db.session.add(user)
         db.session.commit()
+        mail_message("Welcome to watchlist","email/welcome_user",user.email,user=user)
         return redirect(url_for('auth.login'))
     title = "New Account"
     return render_template('auth/register.html',registration_form = form,title=title)
